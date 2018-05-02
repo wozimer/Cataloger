2877135690fbc87c9168eda4d694e344c513aec7
Maxim Remizov <wozimer@gmail.com>
2018-05-02 19:17:08 +0300

    Добавлена кнопка для основного действия Убил фабрику формы, код перенес в класс формы

--- Manager/src/com/wozimer/cataloger/Main.java
+++ Manager/src/com/wozimer/cataloger/Main.java
@@ -1,15 +1,26 @@
 package com.wozimer.cataloger;
 
-import com.wozimer.cataloger.ui.MainFrameFactory;
+import com.wozimer.cataloger.ui.MainFrame;
 
-import java.awt.*;
+import java.util.logging.Logger;
 
 public class Main {
 
-    public static void main(String[] args) {
-        MainFrameFactory mainFrameFactory = new MainFrameFactory();
-        Frame mainFrame = mainFrameFactory.createFrame();
-        mainFrame.setVisible(true);
+    private static final Logger LOGGER = Logger.getLogger("com.wozimer.cataloger.Main");
 
+    public static void main(String[] args) {
+        MainFrame mainFrame = MainFrame.createFrame();
+        mainFrame.addMainFrameListener(new MainFrame.MainFrameListener() {
+            @Override
+            public void onExit(MainFrame mainFrame) {
+
+            }
+
+            @Override
+            public void onNewFiles(MainFrame mainFrame) {
+
+            }
+        });
+        mainFrame.getFrame().setVisible(true);
     }
 }
